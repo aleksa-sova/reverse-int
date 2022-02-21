1303425fa9a54f692cc89b839936826c2a6a1e9c
aleksa-sova <aleksandra13sova@gmail.com>
2022-02-21 19:06:59 +0200

    third commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+
+  n = Math.abs(n);
+
+  let arr1 = n.toString().split('');
+
+  let revers = arr1.reverse();
+
+  let num2 = revers.join('');
+
+  num2 = Number(num2);
+
+  return num2;
+
 }
